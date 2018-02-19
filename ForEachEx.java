6f2921492699d50731bcaf858a4cc20b8bd40bd6
shanthiSalendran <32483054+shanthiSalendran@users.noreply.github.com>
2018-02-19 19:34:29 +0530

    Add files via upload

---
+++ ForEachEx.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+public class ForEachEx {
+
+public static void main(String[] args) {
+		
+		//creating sample Collection
+		List<Integer> myList = new ArrayList<Integer>();
+		for(int i=0; i<10; i++) myList.add(i);
+		
+		//traversing using Iterator
+		Iterator<Integer> it = myList.iterator();
+		while(it.hasNext()){
+			Integer i = it.next();
+			System.out.println("Iterator Value::"+i);
+		}
+		
+		myList.forEach(item->System.out.println(item));
+
+		Map<String, Integer> items = new HashMap<>();
+		items.put("A", 10);
+		items.put("B", 20);
+		items.put("C", 30);
+		items.put("D", 40);
+		items.put("E", 50);
+		items.put("F", 60);
+		
+		//traversing using Entry
+		for (Map.Entry<String, Integer> entry : items.entrySet()) {
+			System.out.println("Item : " + entry.getKey() + " Count : " + entry.getValue());
+		}
+
+		//items.forEach((k,v)->System.out.println("Item : " + k + " Count : " + v));
+
+		items.forEach((k,v)->{
+			System.out.println("Item : " + k + " Count : " + v);
+			if("E".equals(k)){
+				System.out.println("Hello E");
+			}
+		});	
+		
+		
+	}
+
+}
